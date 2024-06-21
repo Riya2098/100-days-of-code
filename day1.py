38c8c45531402b5503ea7bb1f1c05e7f822ebb2d
Riya Verma <riyaverma8503@gmail.com>
2024-06-21 21:21:40 +0530

    my day 1 commit

---
+++ day1.py
@@ -0,0 +1,16 @@
+#1. Create a greeting for your program.
+print("Hello there, Welcome to my first project")
+print ("\n")
+
+
+#2. Ask the user for the city that they grew up in.
+city = input("Tell me, which city did you grew up in? \n")
+
+#3. Ask the user for the name of a pet.
+pet = input ("What's your pet's name? \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+finalname = city + " " + pet
+
+#5. Make sure the input cursor shows on a new line:
+print("Your band's name is: "+finalname)
